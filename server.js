048960e1fc124bc01d5b2d567eecfa817084b1ce
Dominick <Domdominick@hotmail.com>
2020-01-26 05:32:42 -0500

    fixes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-// Routes
+
 // =============================================================
 var routes = require("./controllers/contactController.js");
 app.use(routes);
