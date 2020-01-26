d8d97bf0a15ad2b4b3d359002528863639210aca
Dominick <Domdominick@hotmail.com>
2020-01-26 05:33:43 -0500

    fixes

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ var routes = require("./controllers/contactController.js");
 app.use(routes);
 
 app.listen(PORT, function () {
-  // Log (server-side) when our server has started
+
   console.log("Server listening on: http://localhost:" + PORT);
 });
