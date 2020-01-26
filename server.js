1d31ee820a44de1832c2117974f91d09c48f34de
Dominick <Domdominick@hotmail.com>
2020-01-26 05:33:19 -0500

    fixes

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/contactController.js");
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
