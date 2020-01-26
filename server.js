f134416daefc0437ed5d601624b76a18cd6cf77d
Dominick <Domdominick@hotmail.com>
2020-01-26 04:13:52 -0500

    fixes

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.use(routes);
 // Start our server so that it can begin listening to client requests.
 app.listen(process.env.PORT || 3000, function () {
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + this.address().port, app.settings.env);
+  console.log("Server listening on: https://localhost:" + this.address().port, app.settings.env);
 });
